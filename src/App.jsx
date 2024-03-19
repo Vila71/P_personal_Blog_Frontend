d8a90b23c86b4ef2ab6b58fb7652d13e69713442
Vila71 <lolivila71@gmail.com>
2024-03-19 12:55:31 +0100

    create projet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Router from '../src/configuration/Router'
+
+const App = () => {
+  return (
+    <>
+      <Router />
+    </>
+  )
+}
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blueLight': '#EDF0FF',
+        'blue': '#0025CE',
+        'green': '#00BF63',
+      }
+    },
+  },
+  plugins: [
+    require("flowbite")
+  ],
+}
