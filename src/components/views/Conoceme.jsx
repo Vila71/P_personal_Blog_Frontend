c5a775d6b89db8d7ad821d76030a621b8574eba1
Vila71 <lolivila71@gmail.com>
2024-03-20 20:02:38 +0100

    create Sobremi

--- src/components/views/Conoceme.jsx
+++ src/components/views/Conoceme.jsx
@@ -1,12 +1,15 @@
 // Conoceme.js
 
 import React from 'react';
+import Sobremi from '../molecules/Sobremi';
+
 
 const Conoceme = () => {
   return (
     <div>
       <h2>Conóceme</h2>
       <p>Contenido de la página "Conóceme"...</p>
+      <Sobremi></Sobremi>
     </div>
   );
 }
